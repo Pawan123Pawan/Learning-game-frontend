993fc1dec12762565c86698f1d0f257eed907b91
Pawan123Pawan <105289150+Pawan123Pawan@users.noreply.github.com>
2024-01-22 15:30:29 +0530

    Update UserDashbord.jsx

--- src/pages/user/UserDashbord.jsx
+++ src/pages/user/UserDashbord.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BaseUrl } from "../../services/BaseUrl";
 import EachQuestionEvaluation from "../../component/EachQuestionEvaluation";
-import UserSocre from "./userSocre";
+import UserSocre from "./UserSocre";
 import LoaderSpinner from "../../component/LoaderSpinner";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
